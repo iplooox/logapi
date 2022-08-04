7a0d19704bd29461fc862afb351fbaacc57292bd
iplooox <iplooox@gmail.com>
2022-08-04 21:46:19 +0200

    Add a failing test for logger factory.

---
+++ LogApi/LoggerFactory.cs
@@ -0,0 +1,19 @@
+﻿using LogApi.BusinessObjects.Loggers;
+using LogApi.Enums;
+using ILogger = LogApi.BusinessObjects.Loggers.ILogger;
+
+namespace LogApi;
+
+public static class LoggerFactory
+{
+    public static ILogger CreateLogger(LogDestination destination)
+    {
+        return new KafkaLogger();
+        // switch (destination)
+        // {
+        //     case LogDestination.Kafka:
+        //         return new KafkaLogger();
+        //         break;
+        // }
+    }
+}
--- LogApiTests/LoggersTests.cs
+++ LogApiTests/LoggersTests.cs
@@ -1,10 +1,14 @@
 using FluentAssertions;
+using LogApi;
 using LogApi.BusinessObjects.Loggers;
+using LogApi.Enums;
 
 namespace LogApiTests;
 
 public class LoggersTests
 {
+    private readonly HashSet<int> _allowedLoggerTypes = new() {1,2,3};
+
     [Fact]
     public void LoggerSuccessfullyGetCreatedAndLog()
     {
@@ -16,6 +20,42 @@ public class LoggersTests
 
             logResponse.Success.Should().BeTrue();
             logResponse.Error.Should().BeNull();
+        }
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    public void FactoryReturnCorrectLogger(int destinationInput)
+    {
+        LogDestination destination = (LogDestination)destinationInput;
+
+        var createLogger = () => LoggerFactory.CreateLogger(destination);
+
+        if (!_allowedLoggerTypes.Contains(destinationInput))
+        {
+            createLogger.Should().Throw<Exception>();
+            return;
+        }
+        
+        ILogger logger = LoggerFactory.CreateLogger(destination);
+
+        logger.Destination.Should().Be(destination);
+
+        switch (destination)
+        {
+            case LogDestination.Kafka:
+                (logger is KafkaLogger).Should().BeTrue();
+                break;
+            case LogDestination.FlatFile:
+                (logger is FlatFileLogger).Should().BeTrue();
+                break;
+            case LogDestination.RabbitMQ:
+                (logger is RabbitMqLogger).Should().BeTrue();
+                break;
         }
     }
 }
