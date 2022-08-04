e6713c516ed6638d9716674339b1a7c076ea29fa
iplooox <iplooox@gmail.com>
2022-08-04 22:19:39 +0200

    Added app config for witch logging to use.

---
+++ LogApi/BusinessObjects/Configuration/LogConfigurationProvider.cs
@@ -0,0 +1,28 @@
+﻿using LogApi.Enums;
+
+namespace LogApi.BusinessObjects.Configuration;
+
+public interface ILogConfigurationProvider
+{
+    LogDestination GetLoggerDestination();
+}
+
+public class LogLogConfigurationProvider : ILogConfigurationProvider
+{
+    private readonly IConfiguration _configuration;
+
+    public LogLogConfigurationProvider(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public LogDestination GetLoggerDestination()
+    {
+        if (!int.TryParse(_configuration["LogDestination"], out int destination))
+        {
+            return LogDestination.None;
+        }
+
+        return (LogDestination) destination;
+    }
+}
