08d6de97cd2d9be851c43cd22428eecab099e9b4
iplooox <iplooox@gmail.com>
2022-08-04 21:51:18 +0200

    Add factory logic after all tests succeed

--- LogApi/LoggerFactory.cs
+++ LogApi/LoggerFactory.cs
@@ -8,12 +8,16 @@ public static class LoggerFactory
 {
     public static ILogger CreateLogger(LogDestination destination)
     {
-        return new KafkaLogger();
-        // switch (destination)
-        // {
-        //     case LogDestination.Kafka:
-        //         return new KafkaLogger();
-        //         break;
-        // }
+        switch (destination)
+        {
+            case LogDestination.Kafka:
+                return new KafkaLogger();
+            case LogDestination.FlatFile:
+                return new FlatFileLogger();
+            case LogDestination.RabbitMQ:
+                return new RabbitMqLogger();
+            default:
+                throw new ArgumentException($"Cannot create Logger for type {destination}", nameof(destination));
+        }
     }
 }
