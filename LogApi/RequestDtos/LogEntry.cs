9b8a0928efe43c9bea453463114f50e632d6bd68
iplooox <iplooox@gmail.com>
2022-08-04 21:31:07 +0200

    Basic validation for request dto.

--- LogApi/RequestDtos/LogEntry.cs
+++ LogApi/RequestDtos/LogEntry.cs
@@ -1,14 +1,27 @@
-﻿using LogApi.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using LogApi.Enums;
 
 namespace LogApi.RequestDtos;
 
 public class LogEntryDto
 {
+    [Required]
     public Guid ApplicationId { get; set; }
+
+    [Required]
     public Guid TraceId { get; set; }
-    public Guid RequestId { get; set; }
+    
+    public Guid? RequestId { get; set; }
+    
+    [Required]
+    [Range(0,4)]
     public LogSeverity Severity { get; set; }
+    
+    [Required]
     public DateTime TimeStamp { get; set; }
+    
+    [Required]
     public string? Message { get; set; }
+
     public string? ComponentName { get; set; }
 }
